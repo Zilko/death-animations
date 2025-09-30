5f8f0d192140bf93ede6923dcf2253b80f72d5d1
Zilko <69888540+Zilko@users.noreply.github.com>
2025-09-29 18:25:01 -0600

    gg

--- src/Animations/MrHippo.hpp
+++ src/Animations/MrHippo.hpp
@@ -22,66 +22,35 @@ private:
 
     int m_speech = 1;
 
-    ~MrHippo() {
-        SoundManager::stop();
-
-        if (m_data)
-            delete m_data;
-    }
-
-    # if OP_GNUC_PREREQ(3,0)
-        #  define OP_UNLIKELY(_x) (__builtin_expect(!!(_x),0))
-        # else
-        #  define OP_UNLIKELY(_x) (!!(_x))
-        # endif
-
-            #  define OP_FATAL(_str) (op_fatal_impl(_str,__FILE__,__LINE__))
-
-    #  define OP_ASSERT(_cond) \
-        do{ \
-            if(OP_UNLIKELY(!(_cond)))OP_FATAL("assertion failed: " #_cond); \
-        } \
-        while(0)
-
-    static void op_fatal_impl(const char *_str,const char *_file,int _line){
-        fprintf(stderr,"Fatal (internal) error in %s, line %i: %s\n",
-        _file,_line,_str);
-        abort();
-    }
-
-    static int op_fread(void *_stream,unsigned char *_ptr,int _buf_size){
-        FILE   *stream;
-        size_t  ret;
-        /*Check for empty read.*/
-        if(_buf_size<=0)return 0;
-        stream=(FILE *)_stream;
-        ret=fread(_ptr,1,_buf_size,stream);
-        OP_ASSERT(ret<=(size_t)_buf_size);
-        /*If ret==0 and !feof(stream), there was a read error.*/
-        return ret>0||feof(stream)?(int)ret:OP_EREAD;
+    static int read(void* stream, unsigned char* ptr, int nbytes) {
+        return fread(ptr, 1, nbytes, static_cast<FILE*>(stream));
     }
 
     void playSpeech(float) {
-        int err = 0;
         std::filesystem::path path = Mod::get()->getResourcesDir() / fmt::format("hippo_{}.opus", m_speech);
 
         FILE* fp = fopen(utils::string::pathToString(path).c_str(), "rb");
 
+        if (!fp) return log::error("Mr. Hippo: Couldn't open file. {}", path);
+
         OpusFileCallbacks cb = {
-            op_fread,
+            read,
             NULL,
             NULL,
             (op_close_func)fclose
         };
+        int err = 0;
 
         OggOpusFile* opusFile = op_open_callbacks(fp, &cb, NULL, 0, &err);
 
-        if (err != 0) return log::error("Opus has failed you, boy. {}", err);
+        if (err != 0 || !opusFile) {
+            fclose(fp);
+            return log::error("Opus has failed you, boy. {}", err);
+        }
 
         int channels = op_head(opusFile, 0)->channel_count;
-        // ogg_int64_t pcmTotal = op_pcm_total(opusFile, -1);
-        ogg_int64_t pcmTotal = 9936000;
-        float duration = static_cast<float>(pcmTotal) / 48000.f;
+        float duration = m_duration - 8.f;
+        ogg_int64_t pcmTotal = duration * 48000;
 
         m_data = new OpusData(opusFile, channels);
 
@@ -199,18 +168,31 @@ private:
         );
     }
 
+    float getSpeechDuration(int speech) {
+        switch (m_speech) {
+            case 1: return 207.f;
+            case 2: return 172.f;
+            case 3: return 139.f;
+            case 4: return 140.f;
+        }
+
+        return 207.f;
+    }
+
+    ~MrHippo() {
+        SoundManager::stop();
+
+        if (m_data)
+            delete m_data;
+    }
+
     ANIMATION_CTOR_CREATE(MrHippo) {
         m_speech = Utils::getSettingBool(Anim::MrHippo, "monologue");
 
         if (m_speech == 0)
             m_speech = Utils::getRandomInt(1, 4);
 
-        switch (m_speech) {
-            case 1: m_duration = 215.f; break;
-            case 2: m_duration = 180.f; break;
-            case 3: m_duration = 147.f; break;
-            case 4: m_duration = 148.f; break;
-        }
+        m_duration = getSpeechDuration(m_speech) + 8.f;
 
         m_retryLayerDelay = m_duration - 0.3f;
     }
