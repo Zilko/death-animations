f80619d0e49e3776b01fe318facc597bc98c3067
Zilko <69888540+Zilko@users.noreply.github.com>
2025-10-02 12:48:19 -0600

    erm

--- src/Animations/Terraria.hpp
+++ src/Animations/Terraria.hpp
@@ -2,12 +2,28 @@
 
 class Terraria : public BaseAnimation {
     
+private:
+
+    constexpr static std::array m_deathMessages = std::to_array<std::string_view>({
+        "{} left a small crater.","{} discovered gravity.","{} didn't bounce.","{} faceplanted the ground.","{} fell victim to gravity.","{} fell to their death.","{} hit the ground face first.","{} is shark food.","{} forgot to breathe.","{} is sleeping with the fish.","{} drowned.","{} thought he was a fish.","{} likes to play in magma.","{} got melted.","{} tried to swim in lava.","{} was incinerated.","{} exploded.","{} had a mining accident.","{} forgot to run.","{} was destroyed.","{} was turned into pile of flesh.","{} got snapped in half.","{} was murdered.","{} was mangled.","{}'s vital organs were ruptured.","{}'s plead for death was answered.","{}'s innards were made outards.","{}'s skull was crushed.","{}'s body was mangled.","{} was licked.","{} tried to escape.","{} turned corrupt.","{} was torn in half.","{} was slain...","{} didn't materialize.","{}'s legs appeared where his/her head should be."
+    });
+
     ANIMATION_CTOR_CREATE(Terraria) {}
     
 public:
 
+    void startEarly() override {
+    
+    }
+
     void start() override {
-        // CCSprite::create();
+        log::debug("no");
+        CCLabelBMFont* lbl = CCLabelBMFont::create("", "terraria.fnt"_spr);
+        // lbl->setPosition(m_size / 2.f);
+        
+        // Utils::fixScaleTextureSizexd(lbl);
+        
+        // addChild(lbl);
     }
 
     };
