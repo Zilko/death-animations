e499fbafd95bc94cfbab871bffb9654a59368287
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-03-30 14:42:51 -0400

    More Icons.

--- src/UI/PreviewLayer.cpp
+++ src/UI/PreviewLayer.cpp
@@ -3,7 +3,7 @@
 
 #include "PreviewLayer.hpp"
 
-// #include <hiimjustin000.more_icons/include/MoreIcons.hpp>
+#include <hiimjustin000.more_icons/include/MoreIcons.hpp>
 
 PreviewLayer::~PreviewLayer() {
     if (m_circleWave)
@@ -204,8 +204,8 @@ bool PreviewLayer::init() {
     
     m_mainLayer->addChild(m_player);
     
-    // if (Loader::get()->isModLoaded("hiimjustin000.more_icons"))
-    //     MoreIcons::updateSimplePlayer(m_player, IconType::Cube);
+    if (Loader::get()->isModLoaded("hiimjustin000.more_icons"))
+        more_icons::updateSimplePlayer(m_player, IconType::Cube);
     
     m_time = (m_spike->getPositionX() - m_player->getPositionX()) * 0.003625f;
         
